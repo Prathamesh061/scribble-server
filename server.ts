047c0c9d4ebb9078656a15c37e8c19cd0aca0b63
Prathamesh Lakhpati <prathmeshlakhpaty@gmail.com>
2023-10-21 22:34:56 +0530

    done

--- server.ts
+++ server.ts
@@ -8,11 +8,10 @@ dotenv.config();
 
 const app = express();
 
-// app.use(cors({ origin: process.env.URL }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.URL }));
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: "*" } });
+const io = new Server(httpServer, { cors: { origin: process.env.URL } });
 
 io.on("connection", (socket: Socket) => {
   socket.on("beginPath", (arg: any) => {
@@ -36,6 +35,6 @@ io.on("connection", (socket: Socket) => {
   });
 });
 
-httpServer.listen(3000, "192.168.1.5", () => {
-  console.log(`Server is up and running at http://192.168.1.5:3000`);
+httpServer.listen(process.env.PORT, () => {
+  console.log(`Server is up and running`);
 });
